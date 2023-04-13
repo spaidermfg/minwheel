f0ac075dc33cd45aa46ee3fcb8870a8d52cf89e2
Toymachine <488434442@qq.com>
2023-04-13 23:59:22 +0800

    select

--- select/main.go
+++ select/main.go
@@ -1,7 +1,31 @@
 package main
 
+import (
+	"log"
+	"time"
+)
+
 //基于select的多路复用
+//没有任何case的select{}语句会一直阻塞下去
 
 func main() {
+	abort := make(chan struct{})
+	log.Println("Commencing countdown")
+	c := time.Tick(time.Second * 1)
 
+	for countdown := 10; countdown > 0; countdown-- {
+		log.Println(countdown)
+		select {
+		case <-c:
+			log.Println("--------------------")
+		case <-abort:
+			log.Println("launch abort")
+			return
+		}
+	}
+	launch()
+}
+
+func launch() {
+	log.Println("Commencing countdown!!!")
 }
