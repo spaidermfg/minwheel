fc723abe7ad2519bb6b2e228a6d293d3e4acbd4c
Toymachine <488434442@qq.com>
2023-04-15 23:56:21 +0800

    并发目录遍历

--- channel/walkdir/du.go
+++ channel/walkdir/du.go
@@ -1,1 +1,31 @@
 package walkdir
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+//并发目录遍历
+
+// 递归遍历目录，向通道发送文件的大小
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			s := filepath.Join(dir, entry.Name())
+			walkDir(s, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+// return dir inner info
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+	}
+	return entries
+}
