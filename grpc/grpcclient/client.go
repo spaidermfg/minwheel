389bc7dd60f1a12665485735abb0b5fd9a147766
Toymachine <488434442@qq.com>
2023-06-14 23:51:21 +0800

    grpc

--- grpc/grpcclient/client.go
+++ grpc/grpcclient/client.go
@@ -6,16 +6,52 @@ import (
 )
 
 func main() {
-	client, err := rpc.Dial("tcp", "localhost:7642")
-	if err != nil {
-		log.Fatal("rpc dial error:", err)
-	}
+	// client, err := rpc.Dial("tcp", "localhost:7642")
+	// if err != nil {
+	// 	log.Fatal("rpc dial error:", err)
+	// }
+	//
+	// var reply string
+	// //HelloService.Hello：grpc服务名和方法名
+	// if err := client.Call(HelloServiceName+".Hello", "mark", &reply); err != nil {
+	// 	log.Fatal("rpc connection error:", err)
+	// }
+	//
+	// log.Println(reply)
 
-	var reply string
-	//HelloService.Hello：grpc服务名和方法名
-	if err := client.Call("HelloService.Hello", "mark", &reply); err != nil {
-		log.Fatal("rpc connection error:", err)
-	}
-
-	log.Println(reply)
+   client, err := DialHelloService("tcp", "localhost:7642")
+   if err != nil {
+     log.Fatal("dialing failed", err)
+   }
+  
+   var reply string
+   err = client.Hello("mark", &reply)
+   if err != nil {
+     log.Fatal("hello")
+   }
+   
+   log.Println(reply)
+   
+  
 }
+
+type HelloServiceClient struct {
+  *rpc.Client
+}
+
+//var _ HelloServiceInterface = (*HelloServiceClient) (nil)
+
+func DialHelloService(network, address string) (*HelloServiceClient, error) {
+  c, err := rpc.Dial(network, address)
+  if err != nil {
+    return nil, err
+  }
+
+  return &HelloServiceClient{Client: c}, err
+}
+
+func (p *HelloServiceClient) Hello(request string, reply *string) error {
+  return p.Client.Call(HelloServiceName+".Hello", request, reply)
+}
+
+const HelloServiceName = "path/to/pkg.HelloService"
--- grpc/main.go
+++ grpc/main.go
@@ -12,19 +12,23 @@ import (
 
 func main() {
 	//将对象类型中所有满足rpc规则的方法注册为rpc函数
-	rpc.RegisterName("HelloService", new(HelloService))
+	//	rpc.RegisterName("HelloService", new(HelloService))
+  log.Println("grpc server start...")
+	RegisterHelloService(new(HelloService))
 	listener, err := net.Listen("tcp", ":7642")
 	if err != nil {
 		log.Fatal("Listen tcp error:", err)
 	}
+  
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("accept error:", err)
+		}
 
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("accept error:", err)
+		//在tcp链接上提供rpc服务
+		go rpc.ServeConn(conn)
 	}
-
-	//在tcp链接上提供rpc服务
-	rpc.ServeConn(conn)
 }
 
 type HelloService struct{}
@@ -32,4 +36,14 @@ type HelloService struct{}
 func (h *HelloService) Hello(request string, reply *string) error {
 	*reply = "hello: " + request
 	return nil
+}
+
+const HelloServiceName = "path/to/pkg.HelloService"
+
+type HelloServiceInterface interface {
+	Hello(request string, reply *string) error
+}
+
+func RegisterHelloService(svc HelloServiceInterface) error {
+	return rpc.RegisterName(HelloServiceName, svc)
 }
