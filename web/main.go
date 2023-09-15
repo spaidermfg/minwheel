fb4db9ac09945304ee830c756f3a2799b1ea63de
Toymachine <488434442@qq.com>
2023-09-15 10:18:25 +0800

    net test

--- web/main.go
+++ web/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	deadline()
 	log.Println("start running...")
 	go http.HandleFunc("/trigger", HandleRequest)
 
@@ -21,4 +22,18 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
 	log.Printf("processing of requests from %v...\n", r.URL.Path)
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func deadline() {
+	a := []byte{
+		0x10, 0x20, 0x30, 0x40,
+		0, 0, 0, 110,
+		0x46, 0, 0, 0,
+		0, 0, 0, 0,
+		0, 0, 0, 0,
+		0x0, //will come back
+		0x10, 0x20, 0x30, 0x40,
+	}
+
+	log.Println(string(a))
 }
