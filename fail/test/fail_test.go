6e098944b9e6f54270455b3587706360bde4c531
Toymachine <488434442@qq.com>
2023-09-26 11:38:54 +0800

    shadow

--- fail/test/fail_test.go
+++ fail/test/fail_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"testing"
 )
 
@@ -25,14 +27,24 @@ func TestShadow(t *testing.T) {
 	if err != nil {
 		return
 	}
-	println("i=", i)
+	println("i =", i)
 
 	if i == 11 {
 		//var j int
 		j, err := bar()
 		if err != nil {
+			fmt.Println("err:", err)
 			return
 		}
 		println("j=", j)
 	}
+}
+
+func TestShadowV1(t *testing.T) {
+	var i int
+	i, err := foo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("i:", i)
 }
