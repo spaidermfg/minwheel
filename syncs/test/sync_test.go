1c57ac2d04aec501b3347e2e03ed6221f15f9989
Toymachine <488434442@qq.com>
2023-10-11 09:13:56 +0800

    livelock

--- syncs/test/sync_test.go
+++ syncs/test/sync_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -200,4 +201,64 @@ func TestSyncMap(t *testing.T) {
 	if actual, ok := sm.LoadOrStore("a", true); ok {
 		fmt.Printf("%v is exists\n", actual)
 	}
+}
+
+func TestLiveLock(t *testing.T) {
+	cadence := sync.NewCond(&sync.Mutex{})
+	go func() {
+		for range time.Tick(time.Millisecond * 1) {
+			cadence.Broadcast()
+		}
+	}()
+
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
+	}
+
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprintf(out, ". Success!")
+			return true
+		}
+
+		takeStep()
+		atomic.AddInt32(dir, -1)
+		return false
+	}
+
+	var left, right int32
+	tryLeft := func(out *bytes.Buffer) bool {
+		return tryDir("left", &left, out)
+	}
+
+	tryRight := func(out *bytes.Buffer) bool {
+		return tryDir("right", &right, out)
+	}
+
+	walk := func(walking *sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() {
+			fmt.Println(out.String())
+		}()
+		defer walking.Done()
+
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ {
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tossed her hands up in exasperation!", name)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go walk(&wg, "Alice")
+	go walk(&wg, "Barbara")
+	wg.Wait()
 }
