fe88c424ad736f1a5a3ea34a0f00ae84ab8cf3f0
Toymachine <488434442@qq.com>
2023-11-10 15:29:32 +0800

    netstat

--- netstat/netstat.go
+++ netstat/netstat.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 var tip = flag.String("tip", "", "输入进程相关信息")
@@ -43,9 +44,11 @@ func main() {
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{"序号", "协议", "本地地址", "外部地址", "连接状态", "进程ID", "进程名", "进程路径"})
 
+		fmt.Println(">>> 输出信息开始渲染")
 		for i, o := range n.Output {
 			table.Append([]string{strconv.Itoa(i), o.Proto, o.LocalAddr, o.ForeignAddr, o.State, o.Pid, o.Name, o.Path})
 		}
+		fmt.Println(">>> 输出渲染完成")
 		table.Render()
 		os.Exit(0)
 	}
@@ -53,6 +56,7 @@ func main() {
 }
 
 func (n *netstat) search() {
+	fmt.Println(">>> 开始查询端口使用信息")
 	cmd := exec.Command("cmd", "/C", fmt.Sprintf("netstat -ano | findstr %v", n.Tip))
 	info, err := cmd.Output()
 	if err != nil {
@@ -79,31 +83,41 @@ func (n *netstat) search() {
 			Pid:         fields[4],
 		})
 	}
+	fmt.Println(">>> 端口使用信息查询完成")
 }
 
 func (n *netstat) processName() {
-	for _, o := range n.Output {
-		cmd := exec.Command("powershell", "Get-Process", "-Id", o.Pid, "|", "Select-Object", "*")
-		info, err := cmd.Output()
-		if err != nil {
-			log.Fatal(fmt.Sprintf("命令[%v]执行失败，Err: %v", cmd.String(), err))
-		}
-
-		res := strings.Split(string(info), "\n")
-		for _, v := range res {
-			part := strings.SplitN(strings.TrimSpace(v), ":", 2)
-			if len(part) != 2 {
-				continue
+	fmt.Println(">>> 通过PID开始查询进程信息")
+	var wg sync.WaitGroup
+	for _, v := range n.Output {
+		wg.Add(1)
+		go func(o *output) {
+			defer wg.Done()
+			cmd := exec.Command("powershell", "Get-Process", "-Id", o.Pid, "|", "Select-Object", "*")
+			info, err := cmd.Output()
+			if err != nil {
+				log.Fatal(fmt.Sprintf("命令[%v]执行失败，Err: %v", cmd.String(), err))
 			}
 
-			key := strings.TrimSpace(part[0])
-			value := strings.TrimSpace(part[1])
-			if key == "Path" {
-				o.Path = value
+			res := strings.Split(string(info), "\n")
+			for _, vi := range res {
+				part := strings.SplitN(strings.TrimSpace(vi), ":", 2)
+				if len(part) != 2 {
+					continue
+				}
+
+				key := strings.TrimSpace(part[0])
+				value := strings.TrimSpace(part[1])
+				if key == "Path" {
+					o.Path = value
+				}
+				if key == "ProcessName" {
+					o.Name = value
+				}
 			}
-			if key == "ProcessName" {
-				o.Name = value
-			}
-		}
+		}(v)
 	}
+
+	wg.Wait()
+	fmt.Println(">>> 进程信息查询完成")
 }
