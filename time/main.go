7eccdd9d379b440bc0c4d707ad104248bbc23994
Toymachine <488434442@qq.com>
2023-11-30 17:15:38 +0800

    timer

--- time/main.go
+++ time/main.go
@@ -60,15 +60,62 @@ func main() {
 	// 	}
 	// }
 
-	ticker := time.NewTicker(time.Second * 5)
-	defer ticker.Stop()
+	//ticker := time.NewTicker(time.Second * 5)
+	//defer ticker.Stop()
+	//
+	//// 循环执行定时任务
+	//for {
+	//	select {
+	//	case <-ticker.C:
+	//		// 定时任务的处理逻辑
+	//		fmt.Println("Execute task at", time.Now())
+	//	}
+	//}
 
-	// 循环执行定时任务
-	for {
-		select {
-		case <-ticker.C:
-			// 定时任务的处理逻辑
-			fmt.Println("Execute task at", time.Now())
+	d := new(datetime)
+	d.getTimeNow()
+
+	d.timer()
+}
+
+type datetime struct{}
+
+func (d *datetime) getTimeNow() {
+	now := time.Now()
+	fmt.Println(now)
+}
+
+// 单次定时器
+func (d *datetime) timer() {
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("timer created by after function fired!")
+	})
+
+	timer := time.NewTimer(2 * time.Second)
+	defer timer.Stop()
+	select {
+	case <-timer.C:
+		fmt.Println("timer created by new timer fired!")
+	}
+
+	select {
+	case <-time.After(2 * time.Second):
+		fmt.Println("timer created by after fired!")
+	}
+
+	// 复用定时器
+	// 已经停止的定时器、已经触发过、Timer.C中的数据已被清空
+	time1 := time.NewTimer(3 * time.Second)
+	if !time1.Stop() {
+		<-time1.C
+	}
+	time1.Reset(5 * time.Second)
+
+	select {
+	case <-time1.C:
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Second * 1)
+			fmt.Println("It's done two", i)
 		}
 	}
 }
