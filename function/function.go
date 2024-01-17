6dd8279d7688bb2e804d364c5c5675406399f58a
Toymachine <488434442@qq.com>
2024-01-17 10:12:45 +0800

    func

--- function/function.go
+++ function/function.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"os"
+	"strings"
 )
 
 // 递归：函数直接或间接的调用自身
 
 func main() {
-	parse, err := html.Parse(os.Stdin)
+	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
+	parse, err := html.Parse(strings.NewReader(s))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
@@ -18,6 +20,8 @@ func main() {
 	for _, link := range visit(nil, parse) {
 		fmt.Println(link)
 	}
+
+	outline(nil, parse)
 }
 
 func visit(links []string, n *html.Node) []string {
@@ -33,4 +37,15 @@ func visit(links []string, n *html.Node) []string {
 		links = visit(links, c)
 	}
 	return links
+}
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
 }
