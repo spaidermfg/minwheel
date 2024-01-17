ac8cacf4f06e02bb719e6ced1ec6cfed20c49b80
Toymachine <488434442@qq.com>
2024-01-17 14:51:13 +0800

    func

--- function/function.go
+++ function/function.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -22,6 +23,19 @@ func main() {
 	}
 
 	outline(nil, parse)
+
+	fmt.Println(">>>>", os.Args[1:])
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
 }
 
 func visit(links []string, n *html.Node) []string {
@@ -48,4 +62,24 @@ func outline(stack []string, n *html.Node) {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		outline(stack, c)
 	}
+}
+
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("gettting %s: %s", url, resp.Status)
+	}
+
+	parse, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	return visit(nil, parse), nil
 }
