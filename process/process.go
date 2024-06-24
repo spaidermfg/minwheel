8b988c030602d0ecbca648cec83e96dabeb2b86d
dylan <488434442@qq.com>
2024-06-24 19:33:00 +0800

    feat:启动方法

--- process/process.go
+++ process/process.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/disk"
 	pro "github.com/shirou/gopsutil/v4/process"
 	"log"
@@ -36,6 +37,7 @@ func main() {
 	//wg.Wait()
 	// process()
 	diskInfo()
+	cpuInfo()
 }
 
 func stop(cmd *exec.Cmd, wg *sync.WaitGroup, c chan struct{}) {
@@ -171,4 +173,57 @@ func diskInfo() {
 		"percent:%v\n", d.Total/(1024*1024*1024), d.Free/(1024*1024*1024), d.UsedPercent)
 
 	fmt.Println(d.String())
+}
+
+func cpuInfo() {
+	//c, err := cpu.Info()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println(c)
+	//
+	//percent, err := cpu.Percent(time.Second, false)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	////percentAll, err := cpu.Percent(time.Second, true)
+	////if err != nil {
+	////	log.Fatal(err)
+	////}
+	//
+	//totalMhz := c[0].Mhz
+	//usedMhz := totalMhz * percent[0] / 100.0
+	//fmt.Println(totalMhz, usedMhz, percent, totalMhz/usedMhz)
+	// 获取CPU信息
+	info, err := cpu.Info()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(info)
+	// 计算总的 MHz 和使用的 MHz
+	var totalMHz float64
+	var usedMHz float64
+	for _, v := range info {
+		totalMHz += v.Mhz
+	}
+
+	// 获取总体CPU使用率
+	overallUsage, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	usedMHz = totalMHz * (overallUsage[0] / 100.0)
+
+	// 计算总的 CPU 使用率
+	calculatedUsage := (usedMHz / totalMHz) * 100
+
+	fmt.Printf("Total MHz: %.2f MHz, %v\n", totalMHz, totalMHz)
+	fmt.Printf("Used MHz: %.2f MHz\n", usedMHz)
+	fmt.Printf("Calculated CPU Usage: %.2f%%\n", calculatedUsage)
+	fmt.Printf("Overall CPU Usage from cpu.Percent: %.2f%%\n", overallUsage[0])
 }
